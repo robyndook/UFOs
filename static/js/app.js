1111d549acdf8b257ab8668a2b4967f4ab43face
robyndook <cook.robync@gmail.com>
2022-03-06 21:56:32 -0800

    Add Module Notes

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+// Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  }
+// Functions can call other functions
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+// Converted to an arrow function
+addition = (a, b) => a + b;
+doubleAddition = (c, d) => addition(c, d) * 2;
